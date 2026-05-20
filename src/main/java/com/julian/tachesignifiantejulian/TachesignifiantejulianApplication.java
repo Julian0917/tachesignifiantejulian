6ece2d7d0ab204af484d74de137804080b71d5b5
chen jun hao <chenjunhao@chens-MacBook-Air.local>
2026-05-20 09:00:12 -0400

    Setup

---
+++ src/main/java/com/julian/tachesignifiantejulian/TachesignifiantejulianApplication.java
@@ -0,0 +1,13 @@
+package com.julian.tachesignifiantejulian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachesignifiantejulianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TachesignifiantejulianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julian/tachesignifiantejulian/TachesignifiantejulianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian.tachesignifiantejulian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachesignifiantejulianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
